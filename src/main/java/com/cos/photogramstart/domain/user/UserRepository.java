f24e243d7236e582c38cc5ddb6435ab582a094a6
yyyy2549 <yyyy2549@gmail.com>
2022-06-14 01:22:30 +0900

    로그인 처리

--- src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
+++ src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // 어노테이션이 없어도 JpaRepository를 상속하면 IoC 등록이 자동으로 된다.
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+	//JPA QueryMethod
+	User findByUsername(String username);
+	
 }
